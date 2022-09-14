e5ed9b5732a9169f685a9b5a72a196d5293b7333
thainv <thainv@visionnetwork.vn>
2022-09-14 09:21:43 +0700

    #8 add nodejs

---
+++ nodemailer/sendMail.js
@@ -0,0 +1,19 @@
+// const {google} = require('googleapis');
+// const {nodemailer} = require('nodemailer');
+// require('dotenv').cofing()
+
+// const CLIENT_ID = process.env.CLIENT_ID;
+// const CLIENT_SECRET = process.env.CLIENT_SECRET;
+// const REDIECT_URI = process.env.REDIECT_URI;
+// const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
+
+// const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIECT_URI, REFRESH_TOKEN);
+// oAuth2Client = setCredentials({refresh_token: REFRESH_TOKEN});
+
+// const sentMail = async() => {
+//     try {
+//         const accessToken = await oAuth2Client.getAccessToken();
+//     } catch (error) {
+        
+//     }
+// }
